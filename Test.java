6d4fb3399c6ae7e3eb037d49cdff88b0c7481359
MattaDevi <mdkarthikeyagoud143@gmail.com>
2026-04-29 16:06:09 +0530

    Test.java was updated..

--- Test.java
+++ Test.java
@@ -1,7 +1,22 @@
 public class Test{
    public static void main(String[]args){
+    int a =5;
+    int b = 7;
 
-    System.err.println("Hello World....!");
+    System.out.println(a);
+System.out.println(b);
+
+    try {
+      int result =a/0; //
+    } catch (ArithmeticException e) {
+      e.printStackTrace();
+    }
+   finally{
+
+      System.out.println("I want to reach my destination..!");
+   }
+ 
+    System.out.println("Program ended...");
    }
 
 }
